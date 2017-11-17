9be6a9b5b30923c7cdb134eac79b7a4ada9e99ae
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-11-17 13:01:45 -0200

    fix autocreated rspec tests on models

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "create" do
+    it "default value of price" do
+      product = Product.create(name: 'Bubble')
+      expect(product.price).to eq 0
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "create" do
+    it "valid value" do
+      user = User.create(name: 'Martin')
+      expect(user.name).to eq 'Martin'
+    end
+  end
 end
