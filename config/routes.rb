a93e244d04b298b80d294aca8788e2a7c21d3540
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-11-18 18:06:07 -0200

    created controller cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'cart', to: 'cart#index'
+  get 'cart/new'
+
   resources :products
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
