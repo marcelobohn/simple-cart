dcf3ac2188d9fd8aede5befa238b4a428c850963
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-11-18 13:58:23 -0200

    fix when creating the cart without user

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
   describe "create" do
-    it "simple values" do
+    it "cart with user" do
       user = User.create!(name: 'rick')
       cart = Cart.create(user: user)
       expect(cart.user.name).to eq('rick')
+    end
+
+    it "cart don't have user" do
+      cart = Cart.create()
+      expect(cart.created_at.strftime("%Y-%m-%d")).to eq(Date.today.strftime("%Y-%m-%d"))
     end
   end
 end
